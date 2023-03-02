2875360b356e07ba3ea6989a1eca2ce1a013b63c
emiliaoana <92299329+emiliaoana@users.noreply.github.com>
2023-03-02 15:29:05 +0200

    #2 finished BE project setup (#29)

---
+++ backend/src/main/java/com/openreads/openreads/OpenreadsApplication.java
@@ -0,0 +1,13 @@
+package com.openreads.openreads;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenreadsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenreadsApplication.class, args);
+	}
+
+}
