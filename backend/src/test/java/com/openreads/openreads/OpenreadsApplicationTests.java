5683db2d96a3216eef504b3cce5c9b41a7ba4c10
emiliaoana <92299329+emiliaoana@users.noreply.github.com>
2023-03-02 15:25:19 +0200

    #2 finished BE project setup

---
+++ backend/src/test/java/com/openreads/openreads/OpenreadsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openreads.openreads;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenreadsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
