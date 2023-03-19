468ac56e30ef2b81a5a9bfacc5893d1fcf38c9b4
Dinu Rusu <dinu@codingshadows.com>
2023-03-19 19:52:46 +0200

    oauth2 config & docker stack name (named 'open-reads')

---
+++ backend/src/main/java/com/openreads/openreads/handler/KeycloakLogoutHandler.java
@@ -0,0 +1,35 @@
+package com.openreads.openreads.handler;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class KeycloakLogoutHandler implements LogoutHandler {
+
+    @Override
+    public void logout(HttpServletRequest request, HttpServletResponse response,
+                       Authentication auth) {
+        logoutFromKeycloak((OidcUser) auth.getPrincipal());
+    }
+
+    private void logoutFromKeycloak(OidcUser user) {
+        String endSessionEndpoint = user.getIssuer() + "/protocol/openid-connect/logout";
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(endSessionEndpoint)
+                .queryParam("id_token_hint", user.getIdToken().getTokenValue());
+
+        // ResponseEntity<String> logoutResponse = restTemplate.getForEntity(
+//                builder.toUriString(), String.class);
+//        if (logoutResponse.getStatusCode().is2xxSuccessful()) {
+//            // TODO
+//        } else {
+//            // TODO
+//        }
+    }
+
+}
