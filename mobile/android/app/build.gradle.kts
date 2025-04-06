e1c7280505b5b30b19ed9abe4a8f3fb6971c43e7
Rusu Dinu <dinustefan89.ds@gmail.com>
2025-04-06 18:37:25 +0300

    auth stuff prob breaks ios

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.mobile"
+    namespace = "com.openreads.app"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
 
@@ -21,14 +21,14 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.mobile"
+        applicationId = "com.openreads.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        manifestPlaceholders["appAuthRedirectScheme"] = "com.example.mobile"
+        manifestPlaceholders["appAuthRedirectScheme"] = "com.openreads.app"
     }
 
     buildTypes {
--- mobile/android/app/src/main/kotlin/com/openreads/app/MainActivity.kt
+++ mobile/android/app/src/main/kotlin/com/openreads/app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.mobile
+package com.openreads.app
 
 import io.flutter.embedding.android.FlutterActivity
 
